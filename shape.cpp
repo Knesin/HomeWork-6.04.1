5f228d61dcef813a36dbec2dfe31bd6b7c1352e9
Maksim Kl <GuruHacker@yandex.ru>
2023-08-28 14:45:51 +0300

    Connection switch in Shape

--- shape.cpp
+++ shape.cpp
@@ -10,12 +10,20 @@ Shape::Shape(int _type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, in
 	case line: 
 		x1 = _x1; y1 = _y1;
 		x2 = _x2; y2 = _y2;
+		// считаем площадь фигуры
+		square = 0;
+		// считаем объем фигуры
+		volume = 0;
 		break;
 	case sqr:
 		x1 = _x1; y1 = _y1;
 		x2 = _x2; y2 = _y2;
 		x3 = _x3; y3 = _y3;
 		x4 = _x4; y4 = _y4;
+		// считаем площадь фигуры
+		square = a * b;
+		// считаем объем фигуры
+		volume = 0;
 		break;
 	case cube:
 		x1 = _x1; y1 = _y1; z1 = _z1;
@@ -26,6 +34,10 @@ Shape::Shape(int _type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, in
 		x6 = _x6; y6 = _y6; z6 = _z6; 
 		x7 = _x7; y7 = _y7; z7 = _z7;
 		x8 = _x8; y8 = _y8; z8 = _z8;
+		// считаем площадь фигуры
+		square = 2 * a * b + 2 * a * c + 2 * b * c;
+		// считаем объем фигуры
+		volume = a * b * c;
 		break;
 	default:
 		break;
@@ -35,38 +47,6 @@ Shape::Shape(int _type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, in
 	int a = abs(x1 - x2);
 	int b = abs(y1 - y2);
 	int c = abs(z1 - z2);
-	// считаем площадь фигуры
-	switch (type)
-	{
-	case line:
-		square = 0;
-		break;
-	case sqr:
-		square = a * b;
-		break;
-	case cube:
-		square = 2 * a * b + 2 * a * c + 2 * b * c;
-		break;
-	default:
-		break;
-	}
-
-	// считаем объем фигуры
-	switch (type)
-	{
-	case line:
-		volume = 0;
-		break;
-	case sqr:
-		volume = 0;
-		break;
-	case cube:
-		volume = a * b * c;
-		break;
-	default:
-		break;
-	}
-
 }
 
 Shape::Shape(int type, int _x1, int _y1, double R, double H)
@@ -77,40 +57,21 @@ Shape::Shape(int type, int _x1, int _y1, double R, double H)
 	case circle:
 		x1 = _x1; y1 = _y1;
 		radius = R;
+		// считаем площадь фигуры
+		square = M_PI * R * R;
+		// считаем объем фигуры
+		volume = 0;
 		break;
 	case cylinder:
 		x1 = _x1; y1 = _y1;
 		radius = R;
 		height = H;
-		break;
-	default:
-		break;
-	}
-
-	// считаем площадь фигуры
-	switch (type)
-	{
-	case circle:
-		square = M_PI * R * R;
-		break;
-	case cylinder:
+		// считаем площадь фигуры
 		square = M_PI * R * R + 2 * R * height;
-		break;
-	default:
-		break;
-	}
-
-	// считаем объем фигуры
-	switch (type)
-	{
-	case circle:
-		volume = 0;
-		break;
-	case cylinder:
+		// считаем объем фигуры
 		volume = M_PI * R * R * height;
 		break;
 	default:
 		break;
 	}
-
 }
